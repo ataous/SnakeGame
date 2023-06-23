80276595d2455c13c42e05ac11ae4e42370d7c33
Ata <attaataloo@gmail.com>
2023-06-23 21:41:07 +0330

    Add get & save high score on data.tex

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.high_score = 0
+        self.get_high_score()
         self.hideturtle()
         self.color("white")
         self.penup()
@@ -29,6 +30,7 @@ class Scoreboard(Turtle):
     def rest(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.save_high_score()
         self.score = 0
         self.update()
 
@@ -36,3 +38,11 @@ class Scoreboard(Turtle):
         self.goto(x=0, y=0)
         self.write(arg="GAME OVER", align=ALIGN, font=FONT)
         return
+
+    def get_high_score(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+
+    def save_high_score(self):
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
